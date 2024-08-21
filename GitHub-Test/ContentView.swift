289c11a75d8ddf9f79026356da9c39a94075148f
QuaRang <dbduddnd1225@gmail.com>
2024-08-21 13:58:10 +0900

    추가

---
+++ GitHub-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub-Test
+//
+//  Created by 유영웅 on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub-Test/GitHub_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestApp.swift
+//  GitHub-Test
+//
+//  Created by 유영웅 on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
